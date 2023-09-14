df3b65e8deaa3004dba5bbf649bf8cdd2ea5ef7b
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2023-09-13 19:05:44 -0700

    Version 1.0
    - First Commit to Main
    - Includes code to run local website

---
+++ portfolio/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+#app.config['SECRET_KEY'] = 'b284c64b366d57f0f7b05bf0'
+#db = SQLAlchemy(app)
+#bcrypt = Bcrypt(app)
+#login_manager = LoginManager(app)
+#login_manager.login_view = "login_page"
+#login_manager.login_message_category = "info"
+
+from portfolio import routes
