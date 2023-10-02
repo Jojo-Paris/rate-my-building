dcdcdfa1e62c205d5561ab6f3798af7b505590e0
BinfengJiang22 <binfeng.jiang01@student.csulb.edu>
2023-10-02 16:41:29 -0700

    account_info, update email, change password functions

--- portfolio/routes.py
+++ portfolio/routes.py
@@ -1,10 +1,7 @@
 from portfolio import app
 from flask import render_template, redirect, url_for, flash, request
-from portfolio.models import User, Review
-from portfolio.forms import RegisterForm, LoginForm
-from flask_login import logout_user, login_required
-
-
+from portfolio.models import User
+from portfolio.forms import RegisterForm, LoginForm, UpdateEmailForm, ChangePasswordForm
 from portfolio import db
 from flask_login import login_user, logout_user, login_required, current_user
 import os
@@ -63,3 +60,45 @@ def logout_page():
     logout_user()
     flash("You have been logged out. See you again!", category='info')
     return redirect(url_for("home_page"))
+
+@app.route('/logged-in-home')
+@login_required
+def logged_in_page():
+    return render_template('logged_in.html')
+
+
+@app.route('/update-email', methods=['GET', 'POST'])
+@login_required
+def update_email():
+    form = UpdateEmailForm()
+    if form.validate_on_submit():
+        # Check if the email is already taken
+        existing_user = User.query.filter_by(email_address=form.new_email.data).first()
+        if existing_user:
+            flash('That email is already taken. Please choose a different one.', category='danger')
+            return render_template('update_email.html', form=form)
+        
+        # Update the email_address attribute of the current_user
+        current_user.email_address = form.new_email.data
+        # Commit the changes to the database
+        db.session.commit()
+        flash('Email updated successfully!', category='success')
+        return redirect(url_for('account_info'))
+    return render_template('update_email.html', form=form)
+
+@app.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        # Update the user's password
+        current_user.password = form.new_password.data
+        db.session.commit()
+        flash('Password changed successfully!', category='success')
+        return redirect(url_for('account_info'))
+    return render_template('change_password.html', form=form)
+
+@app.route('/account-info')
+@login_required
+def account_info():
+    return render_template('account_info.html')
