38f5c8e1397c9c588cee975fb5a8bf28e75e6a55
Liam Joseph <liamjoseph.abalos@gmail.com>
2023-10-25 14:50:56 -0700

    test

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == '__main__':
     app.run(debug=True,host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4444)))
+            port=wefwefwef 4444)))
 
 # changes
