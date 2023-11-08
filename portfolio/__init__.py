7c8fa760ac6e5185e32ae5a3e475a4a75ef76dd9
Jodeci (Jojo) Paris <73213153+Jojo-Paris@users.noreply.github.com>
2023-11-08 14:09:56 -0800

    Revert "Merge branch 'main' into Erika"
    This reverts commit f35f14c531ddceb8c305673f535391828947c424, reversing
    changes made to bf097f4028365b6204f56705d7e838e44eb5aad4.

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
-from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
@@ -15,25 +13,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'ratemybuilding0@gmail.com'
-app.config['MAIL_PASSWORD'] = 'doon ilau uzwx fuoj'
-
-mail = Mail(app)
-
 from portfolio import routes
-
-def delete_all_tables():
-    with app.app_context():
-        db.reflect()
-        db.drop_all()
-
-#Deletes all tables...Keep commented
-#delete_all_tables()
-
 
 with app.app_context():
     db.create_all()
--- portfolio/routes.py
+++ portfolio/routes.py
@@ -1,14 +1,48 @@
-import string, secrets
-from portfolio import app, mail, Message, func
+from portfolio import app
 from flask import render_template, redirect, url_for, flash, request
 from portfolio.models import User, Review
-from portfolio.forms import RegisterForm, LoginForm, UpdateEmailForm, ChangePasswordForm, ForgotPasswordForm, ResetPassword, ReviewForm
+from portfolio.forms import RegisterForm
 from portfolio import db
 from flask_login import login_user, logout_user, login_required, current_user
-from datetime import datetime
 import os
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template('home.html')
+
+@app.route("/register", methods=['POST', 'GET'])
+def register_page():
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username = form.username.data,
+            firstName = form.firstName.data,
+            lastName = form.lastName.data,
+            email_address = form.email_address.data,
+            password = form.password1.data
+        )
+
+        db.session.add(user_to_create)
+        db.session.commit()
+
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logge in as {user_to_create.username}", category='success')
+        return redirect(url_for('home_page'))
+
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register.html', form=form)
+
+@app.route("/login", methods=['POST', 'GET'])
+def login_page():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
